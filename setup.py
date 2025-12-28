51d30ea7af4282a3d6c88dfdc7128191ac0acc97
Omar-F-Rashed <omar.f.rashed@gmail.com>
2025-12-28 22:42:43 +0400

    Improvements and documentation for initial public release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
     name='pyauto-desktop',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
