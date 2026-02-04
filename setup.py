948e2c179d9d8e50f3bc029351c3e3cd532452a6
Omar-F-Rashed <omar.f.rashed@gmail.com>
2026-02-04 23:31:43 +0400

    Version update.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 setup(
     name='pyauto-desktop',
-    version='0.4.1',
+    version='0.4.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
